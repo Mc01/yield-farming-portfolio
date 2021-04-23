c7cc162fa14d6fc10c48be24a835cbc6e13ed3c4
Mc <phenom.home@gmail.com>
2021-04-24 00:23:35 +0300

    Fix deploy.

--- deploy.sh
+++ deploy.sh
@@ -8,6 +8,6 @@ git init
 git add -A
 git commit -m 'deploy'
 
-git push -f git@github.com:Mc01/codewars-ranking.git master:gh-pages
+git push -f git@github.com:Mc01/yield-farming-portfolio.git master:gh-pages
 
 cd -
